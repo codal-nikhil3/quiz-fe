f9ba56b2a5fce691c8d423829ce214f2fe0ad422
codal-nikhil3 <nchapadia@codal.com>
2024-03-09 17:06:54 +0530

    0903 Added Add quiz page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Login from './login/login';
 import Quiz from "./quiz/quiz"
+import AddQuestion from './quiz/add_quiz'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login/>} />
         <Route path="/quiz" element={<Quiz />} />
+        <Route path="/add_quiz" element={<AddQuestion />} />
       </Routes>
     </Router>
   );
